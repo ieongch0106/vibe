c49039a6e9ba87a055259b581ff08499b18f4ab0
ieongch0106 <ieong.chou.heng@gmail.com>
2022-04-10 17:03:22 -0400

    Updated the HTML and finished DOM events of signin, signup and location page. Slight work on the back-end, created '../database/user.json' to store user information

---
+++ docs/js/signin.js
@@ -0,0 +1,46 @@
+// import { readFile, writeFile } from 'fs/promises';
+
+const username = document.getElementById('floatingInput')
+const password = document.getElementById('floatingPassword')
+const signin = document.getElementById('signin-btn')
+
+const JSONfile = '../database/user.json'
+
+let user_data = [
+    // { 
+    //     'id': 'aaa@gmail.com'
+    //     'password': '12345678'
+    // }
+]
+
+signin.addEventListener('click', ()=>verify())
+
+
+async function verify() {
+    // user_data = await readFile(JSONfile, { encoding: 'utf8' });
+    const name = username.value
+    const pw = password.value
+    if (userExist(name)) {
+        return verifyPassword(name, pw) ? location.href='home.html' : alert('The password you entered is incorrect') 
+    }
+    else {
+        alert('Username not found')
+    }
+}
+
+function userExist(username) {
+    for (const user of user_data) {
+        if (user.id === username) {
+            return true
+        }
+    }
+    return false
+}
+
+function verifyPassword(username, password) {
+    for (const user of user_data) {
+        if (user.id === username) {
+            return (user.password === password) ? true : false 
+        }
+    }
+}
