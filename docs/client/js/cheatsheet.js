dbb342e3dd671082df03b2061b3aeb01f799734d
MopolyWasHere <99213259+MopolyWasHere@users.noreply.github.com>
2022-04-20 18:32:06 -0400

    Add files via upload

--- docs/client/js/cheatsheet.js
+++ docs/client/js/cheatsheet.js
@@ -1,73 +1,73 @@
 /* global bootstrap: false */
 
-// (function () {
-//   'use strict'
+(function () {
+  'use strict'
 
-//   // Tooltip and popover demos
-//   document.querySelectorAll('.tooltip-demo')
-//     .forEach(function (tooltip) {
-//       new bootstrap.Tooltip(tooltip, {
-//         selector: '[data-bs-toggle="tooltip"]'
-//       })
-//     })
+  // Tooltip and popover demos
+  document.querySelectorAll('.tooltip-demo')
+    .forEach(function (tooltip) {
+      new bootstrap.Tooltip(tooltip, {
+        selector: '[data-bs-toggle="tooltip"]'
+      })
+    })
 
-//   document.querySelectorAll('[data-bs-toggle="popover"]')
-//     .forEach(function (popover) {
-//       new bootstrap.Popover(popover)
-//     })
+  document.querySelectorAll('[data-bs-toggle="popover"]')
+    .forEach(function (popover) {
+      new bootstrap.Popover(popover)
+    })
 
-//   document.querySelectorAll('.toast')
-//     .forEach(function (toastNode) {
-//       var toast = new bootstrap.Toast(toastNode, {
-//         autohide: false
-//       })
+  document.querySelectorAll('.toast')
+    .forEach(function (toastNode) {
+      var toast = new bootstrap.Toast(toastNode, {
+        autohide: false
+      })
 
-//       toast.show()
-//     })
+      toast.show()
+    })
 
-//   // Disable empty links and submit buttons
-//   document.querySelectorAll('[href="#"], [type="submit"]')
-//     .forEach(function (link) {
-//       link.addEventListener('click', function (event) {
-//         event.preventDefault()
-//       })
-//     })
+  // Disable empty links and submit buttons
+  document.querySelectorAll('[href="#"], [type="submit"]')
+    .forEach(function (link) {
+      link.addEventListener('click', function (event) {
+        event.preventDefault()
+      })
+    })
 
-//   function setActiveItem() {
-//     var hash = window.location.hash
+  function setActiveItem() {
+    var hash = window.location.hash
 
-//     if (hash === '') {
-//       return
-//     }
+    if (hash === '') {
+      return
+    }
 
-//     var link = document.querySelector('.bd-aside a[href="' + hash + '"]')
+    var link = document.querySelector('.bd-aside a[href="' + hash + '"]')
 
-//     if (!link) {
-//       return
-//     }
+    if (!link) {
+      return
+    }
 
-//     var active = document.querySelector('.bd-aside .active')
-//     var parent = link.parentNode.parentNode.previousElementSibling
+    var active = document.querySelector('.bd-aside .active')
+    var parent = link.parentNode.parentNode.previousElementSibling
 
-//     link.classList.add('active')
+    link.classList.add('active')
 
-//     if (parent.classList.contains('collapsed')) {
-//       parent.click()
-//     }
+    if (parent.classList.contains('collapsed')) {
+      parent.click()
+    }
 
-//     if (!active) {
-//       return
-//     }
+    if (!active) {
+      return
+    }
 
-//     var expanded = active.parentNode.parentNode.previousElementSibling
+    var expanded = active.parentNode.parentNode.previousElementSibling
 
-//     active.classList.remove('active')
+    active.classList.remove('active')
 
-//     if (expanded && parent !== expanded) {
-//       expanded.click()
-//     }
-//   }
+    if (expanded && parent !== expanded) {
+      expanded.click()
+    }
+  }
 
-//   setActiveItem()
-//   window.addEventListener('hashchange', setActiveItem)
-// })()
+  setActiveItem()
+  window.addEventListener('hashchange', setActiveItem)
+})()
