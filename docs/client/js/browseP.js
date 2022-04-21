b27b24dd62cabac73ec955d6871a130acc97f12d
MopolyWasHere <99213259+MopolyWasHere@users.noreply.github.com>
2022-04-20 23:21:52 -0400

    Add files via upload

---
+++ docs/client/js/browseP.js
@@ -0,0 +1,27 @@
+// const username = document.getElementById('floatingInput')
+// const password = document.getElementById('floatingPassword')
+const explore = document.getElementById('explore');
+
+const explore = document.getElementsByClassName("parties");
+
+async function displayParties() {
+    await fetch(`/home`, {
+        method: 'GET'
+    }).then(response => response.json())
+        .then(data => {
+            // console.log(data);
+            const parties = data;
+            // console.log(parties);
+            const numElem = parties.length > 5 ? 5 : parties.length;
+            for (let i = 0; i < numElem; ++i) {
+                const li = document.createElement('li');
+                const text = document.createTextNode(parties[i].name);
+                li.appendChild(text);
+                explore[0].appendChild(li)
+            }
+        }).catch(error => {
+            console.log(error)
+        })
+}
+
+displayParties();
---
+++ docs/client/js/partyPage.js
@@ -0,0 +1,45 @@
+const partyname = document.getElementById('pname');
+const partyhost = document.getElementById('phost');
+const partytheme = document.getElementById('ptheme');
+const newdiv = document.createElement('div');
+const songSuggest = document.getElementById('songList');
+const submit = document.getElementById('submit');
+
+
+const ls = window.localStorage;
+const lsSet = (k, v) => ls.setItem(k, v);
+
+submit.addEventListener('click', ()=>addSong());
+
+async function addSong() {
+    if (pw.length >= 8) {
+        await fetch(`/user/register?id=${id}&password=${pw}`, {
+            method: "POST"
+        }).then(response => response.json())
+        .then(data => {
+            if (data.Status === 'Success') {
+                console.log(JSON.stringify(data.Status))
+                location.href='location.html'
+                lsSet('user', JSON.stringify( {'id': id} ))
+                const songchoice = document.createTextNode(JSON.stringify(data));
+                await fetch(`https://api.spotify.com/v1/tracks/{id}`, {
+                    method: "GET"
+                }).then(data1 => {
+                    if(data1 === songchoice.value){
+                        newdiv.appendChild(songchoice);
+                        document.body.insertBefore(newDiv, songSuggest);
+                    }else{
+                        alert('Song Does not Exist!')
+                    }
+                })
+
+            } else {
+                alert(JSON.stringify(data.Error))
+            }
+        }).catch(error => {
+            console.log(error)
+        })
+    } else {
+        alert('Song Does not Exist!')
+    }
+}
