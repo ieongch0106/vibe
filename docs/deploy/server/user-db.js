5ffcc1c92146b90f804411e673864842da5955b2
ieongch0106 <ieong.chou.heng@gmail.com>
2022-05-07 21:11:46 -0400

    deploy

---
+++ docs/deploy/server/user-db.js
@@ -0,0 +1,65 @@
+import 'dotenv/config';
+import { MongoClient, ServerApiVersion } from 'mongodb';
+
+export class VibeDatabase {
+    constructor(dburl) {
+        this.dburl = dburl;
+    }
+
+    async connect() {
+        this.client = await MongoClient.connect(this.dburl, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            serverApi: ServerApiVersion.v1,
+        });
+
+        this.db = this.client.db('Vibe');
+
+        await this.init();
+    }
+
+    async init() {
+        this.user = this.db.collection('user');
+    }
+
+    async addUser(username, password) {
+        const check = await this.findUser(username);
+        if (!check) {
+            await this.user.insertOne({username, password});
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    async validatePassword(name, pwd) {
+        const res = await this.user.find({username: name, password: pwd}).toArray();
+        if (res.length !== 0) {
+            return true;
+        }
+        return false;
+    }
+
+    async findUser(name) {
+        const res = await this.user.find({username: name}).toArray();
+        if (res.length !== 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    async addZipCode(name, zipCode) {
+        const user = await this.user.find({username: name}).toArray();
+        this.user.update({_id: user[0]._id}, {$set: {'zipcode': zipCode}});
+        return user
+    }
+
+    async readUser() {
+        return await this.user.find({}).toArray();
+    }
+
+    async close() {
+        this.client.close();
+    }
+}
